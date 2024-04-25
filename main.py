c8e91784be4ddd8f7e22411fa9933852ac4a3b26
simon1655 <simon.izquierdo@urosario.edu.co>
2024-04-25 18:21:33 -0500

    base de datos de 700 libros segun su categoria funciones para extraer los datos y crear la base datos crear la base de datos con 2 categorias

---
+++ main.py
@@ -0,0 +1,22 @@
+import funciones
+
+categoria_1 = funciones.buscar_por_categoria("Science & Technology", 100)
+status, error = funciones.create_book_database(categoria_1)
+if status:
+    print("Libros guardados correctamente.")
+else:
+    print("Los libros no se pudieron guardar: ", error)
+
+categoria_2 = funciones.buscar_por_categoria("Science Fiction & Fantasy", 100)
+status, error = funciones.create_book_database(categoria_2)
+if status:
+    print("Libros guardados correctamente.")
+else:
+    print("Los libros no se pudieron guardar: ", error)
+
+
+
+
+
+
+
