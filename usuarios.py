5491bd599700ca8311c4f218567f94ab23c424bc
juanfecerquera <pipemartinez2006@gmail.com>
2024-05-18 13:57:40 -0500

    ini

--- usuarios.py
+++ usuarios.py
@@ -5,8 +5,6 @@ class User:
 
     def check_password(self, password):
         return self.password == password
-
-# Usuarios de ejemplo
 users = {
     "usuario1": User(username="usuario1", password="contraseña1"),
     "usuario2": User(username="usuario2", password="contraseña2")
